9c2823e6fa51fc3ddf91f75d7d2f9bded1ded7c5
Riccardo <63930826+RiccardoGM@users.noreply.github.com>
2023-11-14 17:32:23 +0100

    Added RL experiment on 2-layer NN

--- SyntheticDataExperiments/Modules/DeepRLAttacks.py
+++ SyntheticDataExperiments/Modules/DeepRLAttacks.py
@@ -26,7 +26,7 @@ class EnvironmentPerceptron(gym.Env):
 
     def __init__(self, x_arr, batch_size, w_stud_0, w_teach, w_target, a_min,
                  a_max, learning_rate, control_cost_weight, activation='Erf',
-                 randomise_initial_condition=False, shuffle_array=False, time_window=500):
+                 randomise_initial_condition=False, shuffle_array=True):
 
         super(EnvironmentPerceptron, self).__init__()
 
@@ -35,7 +35,6 @@ class EnvironmentPerceptron(gym.Env):
         self.batch_size = batch_size
         self.n_timesteps = int(x_arr.shape[0]/batch_size)
         self.learning_rate = learning_rate
-        self.time_window = time_window
         self.control_cost_weight = control_cost_weight
         self.activation = activation
         self.w_teach = w_teach
@@ -141,10 +140,173 @@ class EnvironmentPerceptron(gym.Env):
                 'Per. cost': per_cost,
                 'Nef cost': nef_cost}
 
+        # Observation
         return_obs = np.concatenate((self.w_stud.reshape(1,-1), self.x_batch), axis=0, dtype=np.float32).flatten()
 
         return return_obs, reward, terminated, truncated, info
 
+
+# ************* 2-layer NN (NN2L) ***************** #
+
+class EnvironmentNN2L(gym.Env):
+
+    def __init__(self, observation_type, x_arr, batch_size, W_teach, v_teach, W_target, v_target,
+                 W_stud_0, v_stud_0, a_min, a_max, learning_rate, control_cost_weight, activation='Erf',
+                 outputscaling='inv_sqroot', hlsize=100, shuffle_array=True, train_first_layer=True):
+
+        super(EnvironmentNN2L, self).__init__()
+
+        # Input data to self
+        self.dim_input = x_arr.shape[1]
+        self.batch_size = batch_size
+        self.n_timesteps = int(x_arr.shape[0]/batch_size)
+        self.learning_rate = learning_rate #.astype(np.float32)
+        self.control_cost_weight = control_cost_weight #.astype(np.float32)
+        self.activation = activation
+        self.outputscaling = outputscaling
+        self.train_first_layer = train_first_layer
+        self.hlsize = hlsize
+        self.W_teach = W_teach.astype(np.float32)
+        self.v_teach = v_teach.astype(np.float32)
+        self.W_target = W_target.astype(np.float32)
+        self.v_target = v_target.astype(np.float32)
+
+        # Set time
+        self.timestep = 0
+
+        # Initialize the student
+        self.W_stud_0 = W_stud_0
+        self.v_stud_0 = v_stud_0
+        self.W_stud = self.W_stud_0
+        self.v_stud = self.v_stud_0
+
+        # Data stream
+        self.shuffle_array = shuffle_array
+        if shuffle_array:
+            np.random.shuffle(x_arr)
+        self.x_arr = x_arr.astype(np.float32)
+        self.x_batch = self.x_arr[self.timestep*self.batch_size:(self.timestep+1)*self.batch_size,:]
+        self.x_batch_next = self.x_arr[(self.timestep+1)*self.batch_size:(self.timestep+2)*self.batch_size,:]
+
+        # Define action and observation space
+        self.a_min = a_min
+        self.a_max = a_max
+        self.action_space = gym.spaces.Box(low=self.a_min, high=self.a_max, shape=(1,), dtype=np.float32)
+        self.observation_type = observation_type
+        high = 1e9
+        if self.observation_type=='full':
+            self.observation_space = gym.spaces.Box(low=-high, high=high,
+                                                    shape=(self.dim_input*(1+self.hlsize)+self.hlsize,),
+                                                    dtype=np.float32)
+        elif self.observation_type=='lastlayer':
+            self.observation_space = gym.spaces.Box(low=-high, high=high,
+                                                    shape=(self.dim_input+self.hlsize,),
+                                                    dtype=np.float32)
+        elif self.observation_type=='input':
+            self.observation_space = gym.spaces.Box(low=-high, high=high,
+                                                    shape=(self.dim_input,),
+                                                    dtype=np.float32)
+        else:
+            raise ValueError('observation must be one of the following: full, lastlayer, input.')
+
+
+    def reset(self, seed=0):
+        # Re-initialise the student
+        self.W_stud = self.W_stud_0
+        self.v_stud = self.v_stud_0
+        # Shuffle array
+        if self.shuffle_array:
+            np.random.shuffle(self.x_arr)
+        # Reset time
+        self.timestep = 0
+        # Reset batches
+        self.x_batch = self.x_arr[self.timestep*self.batch_size:(self.timestep+1)*self.batch_size,:]
+        self.x_batch_next = self.x_arr[(self.timestep+1)*self.batch_size:(self.timestep+2)*self.batch_size,:]
+        return_info = {'Shuffle input data': self.shuffle_array}
+
+        if self.observation_type=='full':
+            return_obs = np.concatenate((self.v_stud.reshape(1,-1),
+                                         self.W_stud.reshape(1,-1),
+                                         self.x_batch.reshape(1,-1)), axis=1,
+                                         dtype=np.float32).flatten()
+        elif self.observation_type=='lastlayer':
+            return_obs = np.concatenate((self.v_stud.reshape(1,-1),
+                                         self.x_batch.reshape(1,-1)), axis=1,
+                                         dtype=np.float32).flatten()
+        elif self.observation_type=='input':
+            return_obs = self.x_batch.flatten()
+        return return_obs, return_info
+
+
+    def student_update(self, a):
+
+        W_stud_next, v_stud_next = GA.student_update_NN2L(W_stud=self.W_stud,
+                                                          v_stud=self.v_stud,
+                                                          W_target=self.W_target,
+                                                          v_target=self.v_target,
+                                                          W_teach=self.W_teach,
+                                                          v_teach=self.v_teach,
+                                                          x_batch=self.x_batch,
+                                                          a=a,
+                                                          eta=self.learning_rate,
+                                                          dim_input=self.dim_input,
+                                                          activation=self.activation,
+                                                          output_scaling=self.outputscaling,
+                                                          train_first_layer=self.train_first_layer)
+        return W_stud_next[0], v_stud_next[0]
+
+
+    def step(self, action):
+
+        # Perturbation cost
+        per_cost = self.control_cost_weight * 0.5 * action**2
+
+        # Nefarious cost
+        label_o = GA.NN2L(self.x_batch,
+                          self.W_target,
+                          self.v_target,
+                          activation=self.activation,
+                          output_scaling=self.outputscaling)
+        label_s = GA.NN2L(self.x_batch,
+                          self.W_stud,
+                          self.v_stud,
+                          activation=self.activation,
+                          output_scaling=self.outputscaling)
+        nef_cost = 0.5 * np.mean((label_s-label_o)**2)
+
+        # Update
+        self.W_stud, self.v_stud = self.student_update(action)
+        self.timestep += 1
+        self.x_batch = self.x_arr[self.timestep*self.batch_size:(self.timestep+1)*self.batch_size,:]
+        self.x_batch_next = self.x_arr[(self.timestep+1)*self.batch_size:(self.timestep+2)*self.batch_size,:]
+
+        # Total cost and reward
+        total_cost = per_cost + nef_cost
+        reward = float(-total_cost)
+
+        # End episode flag
+        terminated = bool(self.timestep > self.n_timesteps-2)
+        truncated = False
+
+        # Info
+        info = {'Action': action,
+                'Per. cost': per_cost,
+                'Nef cost': nef_cost}
+
+        # Observation
+        if self.observation_type=='full':
+            return_obs = np.concatenate((self.v_stud.reshape(1,-1),
+                                         self.W_stud.reshape(1,-1),
+                                         self.x_batch.reshape(1,-1)), axis=1,
+                                         dtype=np.float32).flatten()
+        elif self.observation_type=='lastlayer':
+            return_obs = np.concatenate((self.v_stud.reshape(1,-1),
+                                         self.x_batch.reshape(1,-1)), axis=1,
+                                         dtype=np.float32).flatten()
+        elif self.observation_type=='input':
+            return_obs = self.x_batch.flatten()
+
+        return return_obs, reward, terminated, truncated, info
 
 
 # ************************************************* #
@@ -225,6 +387,87 @@ def exp_rl_perceptron(model, env, x_test):
     # Results dictionary
     results = {}
     results['w_dynamics'] = w_stud_dynamics
+    results['a_dynamics'] = a_dynamics
+    results['nef_cost_dynamics'] = nef_cost_dynamics
+    results['per_cost_dynamics'] = per_cost_dynamics
+    results['cum_cost_dynamics'] = cum_cost_dynamics
+    results['d_dynamics'] = d_dynamics
+    results['accuracy_dynamics'] = acc_dynamics
+
+    return results
+
+
+# ************* 2-layer NN (NN2L) ***************** #
+
+def exp_rl_NN2L(model, env, x_test):
+
+    # Parameters from env
+    dim_input = env.dim_input
+    batch_size = env.batch_size
+    n_timesteps = env.n_timesteps
+    learning_rate = env.learning_rate
+    control_cost_weight = env.control_cost_weight
+    activation = env.activation
+    outputscaling = env.outputscaling
+    train_first_layer = env.train_first_layer
+    hlsize = env.hlsize
+    W_teach = env.W_teach
+    v_teach = env.v_teach
+    W_target = env.W_target
+    v_target = env.v_target
+
+    # Target distance and target labels
+    n_samples_test = x_test.shape[0]
+    label_t_test = GA.NN2L(x_test, W_teach, v_teach, activation=activation, output_scaling=outputscaling)
+    label_o_test = GA.NN2L(x_test, W_target, v_target, activation=activation, output_scaling=outputscaling)
+    error_target_teach = np.mean((label_o_test-label_t_test)**2)
+    d_target_teach = 0.5 * error_target_teach
+    pref_control_cost_weight = (2*d_target_teach)
+    t_pred_test = np.sign(label_t_test)
+
+    # Arrays to save dynamics
+    a_dynamics = np.zeros(n_timesteps-1)
+    nef_cost_dynamics = np.zeros(n_timesteps-1)
+    per_cost_dynamics = np.zeros(n_timesteps-1)
+    cum_cost_dynamics = np.zeros(n_timesteps-1)
+    d_dynamics = np.zeros(n_timesteps-1)
+    acc_dynamics = np.zeros(n_timesteps-1)
+
+    # Reset environment
+    observation, _ = env.reset()
+
+    # Run experiment
+    for step in range(n_timesteps):
+        # State, distance, accuracy
+        W_stud = env.W_stud
+        v_stud = env.v_stud
+        x_batch = env.x_batch
+        label_s_test = GA.NN2L(x_test, W_stud, v_stud, activation=activation, output_scaling=outputscaling)
+        s_pred_test = np.sign(label_s_test)
+        d_student_teach = 0.5 * np.mean((label_s_test-label_t_test)**2)
+        d_dynamics[step] = (d_student_teach/d_target_teach)**0.5
+        acc_dynamics[step] = np.sum(s_pred_test==t_pred_test.reshape(1,-1), axis=1)/n_samples_test
+
+        # Nefarious cost
+        label_s = GA.NN2L(x_batch, W_stud, v_stud, activation=activation, output_scaling=outputscaling)
+        label_o = GA.NN2L(x_batch, W_target, v_target, activation=activation, output_scaling=outputscaling)
+        nef_cost_dynamics[step] = 0.5 * np.mean((label_s-label_o)**2)
+
+        # Step
+        action, _ = model.predict(observation, deterministic=True)
+        observation, reward, terminated, truncated, info = env.step(action)
+
+        # Action and costs
+        a_dynamics[step] = action
+        per_cost_dynamics[step] = 0.5 * control_cost_weight * action**2
+        cum_cost_dynamics[step] = per_cost_dynamics[step] + nef_cost_dynamics[step]
+
+        # End of episode
+        if terminated==True:
+            break
+
+    # Results dictionary
+    results = {}
     results['a_dynamics'] = a_dynamics
     results['nef_cost_dynamics'] = nef_cost_dynamics
     results['per_cost_dynamics'] = per_cost_dynamics
---
+++ SyntheticDataExperiments/Parameters/ParametersAttacksComparison_2LayerNN.py
@@ -0,0 +1,69 @@
+# Import statements
+import numpy as np
+import torch
+
+# Model
+model_name = '2LNN'
+activation = 'Erf'
+output_scaling = 'inv_sqroot' # 'inv_sqroot' (default) or 'inv'
+hiddenlayer_width = 100 # 100
+target_type = 'FlippedTeacher' # 'FlippedTeacher' (default) 'Random'
+train_first_layer = True
+
+# Input data parameters
+dim_input = 10
+batch_size = 1
+mu_x = 0
+sigma_x = 1
+n_runs_experiments = 10
+
+# Dynamics parameters
+learning_rate = 2 * 1e-2 * dim_input
+gamma = 0.995
+beta = -np.log(gamma)/dim_input
+
+# N. samples
+n_timesteps = 5000
+n_timesteps_transient_th = 5000 #20000
+n_timesteps_past = 2*n_timesteps_transient_th
+n_samples_average = 200
+n_samples_buffer = 4*n_samples_average
+n_samples_test = n_timesteps
+time_window = 1000
+
+# Control parameters
+a_min = -0 # 0
+a_max = 1-a_min #1
+n_a_gridpoints = 21
+n_runs_calibration = 10
+control_cost_weight = 1.
+greedy_weight_future = dim_input/learning_rate
+opt_pref = True
+fut_pref = 2
+
+# DeepRL Agent
+agent_model_name = 'TD3'
+n_actions = 1
+use_action_noise = True
+action_noise_mean = np.zeros(n_actions)
+action_noise_std = .2 * np.ones(n_actions)
+observation_type = 'lastlayer' # lastlayer, input, full
+use_small_achitecture = False
+shuffle_array = True
+learning_rate_agent = 0.0001
+activation_fn = torch.nn.Tanh
+n_episodes = 8
+save_freq = 1000
+train_freq = 100
+
+# Strings/paths
+local_path = '/Users/riccardo/Documents/GitHub/' #'path_to_progect_folder/'
+export_path = local_path + 'OptimalControlAttacks/SyntheticDataExperiments/Results/StrategiesComparison/2LayerNN/'
+values = (dim_input, hiddenlayer_width, output_scaling, batch_size, a_min, a_max, gamma*1000, 100*learning_rate, 
+          n_runs_experiments, n_runs_calibration, observation_type)
+experiment_description = '_dinput#%d_hlayerw#%d_outputscaling#%s_batchsize#%d_amin#%d_amax#%d_gamma1000#%d_lrpref100#%d_nav#%d_navopt#%d_obstype#%s' % values
+rlmodels_path = local_path + 'OptimalControlAttacks/SyntheticDataExperiments/RLAgents/2LayerNN/'
+agent_model_fullname = 'Agent#%s__@@@_' % agent_model_name + experiment_description
+agent_replaybuffer_fullname = 'RepBuffer#%s__@@@_' % agent_model_name + experiment_description
+path_agent = rlmodels_path + agent_model_fullname
+path_repbuffer = rlmodels_path +  agent_replaybuffer_fullname
